8f91243d4fbc77be228069af6c10a66ad0f25a82
Kyle Bustard <bustardkyle@gmail.com>
2017-09-29 15:25:13 -0500

    create User schema in Accounts context

---
+++ lib/kyle/accounts/user.ex
@@ -0,0 +1,21 @@
+defmodule Kyle.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Kyle.Accounts.User
+
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:name, :username])
+    |> validate_required([:name, :username])
+    |> unique_constraint(:username)
+  end
+end
